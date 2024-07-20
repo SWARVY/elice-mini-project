9ac17324b78984b526a2fbd49dce72adc8a742b7
SWARVY <swarvy0826@naver.com>
2024-07-20 23:57:54 +0900

    chore: eslint-prettier 충돌되는 속성에 대하여 eslint 설정 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,6 +12,7 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
+    'prettier/prettier': 'error',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
