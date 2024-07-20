d578259b0313e3887740b688773c519fa8e8a02e
SWARVY <swarvy0826@naver.com>
2024-07-21 01:00:51 +0900

    chore: 신규 shared 컴포넌트 추가 시 누락된 index.ts 반영

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,2 +1,5 @@
 export * from './Card';
-export * from './Tag';
+export { default as Tag } from './Tag';
+export { default as NoResult } from './NoResult';
+export { default as Loading } from './Loading';
+export { default as Error } from './Error';
