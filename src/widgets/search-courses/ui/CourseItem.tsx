5a756c701b4fdfd83c5bd930662847c19e083f69
SWARVY <swarvy0826@naver.com>
2024-07-21 01:44:03 +0900

    feat: CourseItem의 footer 영역에 price에 따라 가격을 표시하도록 구현

--- src/widgets/search-courses/ui/CourseItem.tsx
+++ src/widgets/search-courses/ui/CourseItem.tsx
@@ -1,6 +1,7 @@
 import Card from '@shared/ui/Card.tsx';
 import { Course } from '@widgets/search-courses';
 
+// props.tags를 통해서 태그들을 확인하고 해당하는게 있다면 해당 내용 표시, 없으면 미분류
 export default function CourseItem({ ...props }: Course) {
   return (
     <Card>
@@ -11,12 +12,23 @@ export default function CourseItem({ ...props }: Course) {
       />
       <Card.ContentWrapper>
         <Card.Description>
-          <p className="font-bold">{props.title}</p>
+          <p></p>
+          <p className="line-clamp-2 font-bold">{props.title}</p>
           <p className="mt-2 line-clamp-2 text-sm text-gray-500">
             {props.short_description}
           </p>
         </Card.Description>
-        <Card.Footer></Card.Footer>
+        <Card.Footer>
+          <>
+            {Number(props.price) ? (
+              <p className="font-bold">
+                ₩{Number(props.price).toLocaleString()}
+              </p>
+            ) : (
+              <p className="font-bold text-green-600">무료</p>
+            )}
+          </>
+        </Card.Footer>
       </Card.ContentWrapper>
     </Card>
   );
