4a7f2eb6a7ccd6fa86aae889e683ff58f35904bb
SWARVY <swarvy0826@naver.com>
2024-07-21 01:40:19 +0900

    chore: 폰트 에셋 추가 및 tailwind config 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-      './src/**/*.{ts,tsx,js,jsx}'
-  ],
+  content: ['./src/**/*.{ts,tsx,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Pretendard"', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
-
+};
