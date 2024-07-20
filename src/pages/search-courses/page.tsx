a501eeeb8bfb8c9b0c38e6d262c44f5141b3e19a
SWARVY <swarvy0826@naver.com>
2024-07-20 23:56:17 +0900

    feat: app.tsx에 라우팅 적용 및 search-courses 페이지 구현

---
+++ src/pages/search-courses/page.tsx
@@ -0,0 +1,9 @@
+import { CourseLayout } from '@widgets/search-courses';
+
+export default function SearchCoursesPage() {
+  return (
+    <div className="max-w-6xl p-6">
+      <CourseLayout />
+    </div>
+  );
+}
