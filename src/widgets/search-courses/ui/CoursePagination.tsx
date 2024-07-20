bdda4da194fff673ecb1b1528a4d735ac7f899b8
SWARVY <swarvy0826@naver.com>
2024-07-21 00:06:28 +0900

    refactor: 페이지네이션이 쿼리 스트링을 초기화하지 않고 page 쿼리 스트링만 조정하도록 수정

--- src/widgets/search-courses/ui/CoursePagination.tsx
+++ src/widgets/search-courses/ui/CoursePagination.tsx
@@ -1,6 +1,7 @@
 import { cn } from '@shared/util';
 import { usePagination } from '@widgets/search-courses';
-import { Link } from 'react-router-dom';
+import { useCallback } from 'react';
+import { useNavigate, useSearchParams } from 'react-router-dom';
 
 interface CoursePaginationProps {
   currentPage: number;
@@ -12,20 +13,33 @@ export default function CoursePagination({
   size,
 }: CoursePaginationProps) {
   const pagination = usePagination({ currentPage, size });
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
+  const paginationButtonHandler = useCallback(
+    (page: number) => {
+      searchParams.delete('page');
+      searchParams.set('page', page.toString());
+      navigate(`/?${searchParams.toString()}`);
+    },
+    [searchParams],
+  );
 
   return (
     <div className="mt-6 flex w-full justify-center gap-x-2">
       {pagination.map((page) => (
-        <div
+        <button
+          type="button"
           key={page}
           className={cn(
             'flex font-extralight size-6 text-sm items-center justify-center rounded-md p-2',
             page === currentPage
               ? 'bg-purple-950 text-white'
               : 'text-gray-500 hover:bg-purple-200 hover:font-bold hover:text-purple-950',
-          )}>
-          <Link to={`/?page=${page}`}>{page}</Link>
-        </div>
+          )}
+          onClick={() => paginationButtonHandler(page)}>
+          {page}
+        </button>
       ))}
     </div>
   );
