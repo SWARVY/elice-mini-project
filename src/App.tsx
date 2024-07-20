e65b274aaff064a08696c8073ddf2b07fd6b3390
SWARVY <swarvy0826@naver.com>
2024-07-21 01:06:07 +0900

    style: html 전체에 배경 색상 적용

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="flex justify-center bg-gray-50">
+    <div className="flex justify-center">
       <Providers>
         <Routes>
           <Route path="/:page?" element={<SearchCoursesPage />} />
