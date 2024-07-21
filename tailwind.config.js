e251039667be8f5d50811766f676b3369181e1da
SWARVY <swarvy0826@naver.com>
2024-07-21 15:52:03 +0900

    chore: Pretendard light 폰트 및 엘리스 컬러 디자인토큰 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,18 @@ export default {
       fontFamily: {
         sans: ['"Pretendard"', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        'elice-lightest-violet': '#EFEFF6',
+        'elice-light-violet': '#7875C8',
+        'elice-violet': '#524FA1',
+        'elice-violet-hover': '#423F8C',
+        'elice-light-gray': '#F9FAFC',
+        'elice-gray': '#F0F1F3',
+        'elice-gray-hover': '#E1E2E4',
+        'elice-dark-gray': '#5E5F61',
+        'elice-darkest-gray': '#222222',
+        'elice-green': '#00AB53',
+      },
     },
   },
   plugins: [],
