655fd908626f296d0646e50b17fcd8d6b5241e2f
SWARVY <swarvy0826@naver.com>
2024-07-21 16:00:54 +0900

    style: 태그 폰트 두께, hover시 두께 - 컬러를 최대한 원 사이트와 유사하게 수정

--- src/shared/ui/Tag.tsx
+++ src/shared/ui/Tag.tsx
@@ -13,7 +13,7 @@ export default function Tag({ title, callback, selected }: TagProps) {
         'm-2 min-h-[30px] rounded-full px-3 py-1 transition-colors text-sm',
         selected
           ? 'text-white bg-elice-violet hover:bg-elice-violet-hover'
-          : 'bg-elice-gray text-elice-dark-gray hover:bg-elice-gray-hover',
+          : 'bg-elice-gray font-normal hover:font-medium text-elice-dark-gray hover:text-elice-darkest-gray hover:bg-elice-gray-hover',
       )}
       type="button"
       onClick={callback}>
